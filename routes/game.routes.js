7c9b57e02ab8d8a78d6ac84d9787fc992f313955
Ali Zaini <azaini@uno.edu>
2020-10-13 15:21:21 -0500

    New Portfolio Template

--- routes/game.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const {getGame, getProduct} = require('../controllers/game.controllers');
-
-
-router.get('/product', getProduct);
-router.get('/:id', getGame);
-
-module.exports = router;
