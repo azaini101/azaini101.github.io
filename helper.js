91e9bc88f4e35b994a5a53e377481445091f145d
Ali Zaini <azaini@uno.edu>
2020-10-15 03:54:44 -0500

    Created Blog Section

--- helper.js
+++ helper.js
@@ -53,17 +53,17 @@ function sortListZA() {
 
 function toggleButtonText() {
     var button = document.getElementById('alphasort');
-    if(button.innerText == "A-Z"){
-        button.innerText = "Z-A";
-        sortListAZ()
+    if(button.innerText == "Newest to Oldest"){
+        button.innerText = "Oldest to Newest";
+        sortByDateNew();
     }
     else{
-        button.innerText = "A-Z";
-        sortListZA()
+        button.innerText = "Newest to Oldest";
+        sortByDateOld();
     }
 }
 
-function sortByDate(){
+function sortByDateNew(){
     sortListAZ()
     var list, i, b, y1, m1, d1;
     list = document.getElementById("myUL");
@@ -76,7 +76,7 @@ function sortByDate(){
         {
             shouldSwitch = false;
             var string = b[i].innerHTML;
-            string = string.substring(string.indexOf(":")+2, string.indexOf("."));
+            string = string.substring(string.lastIndexOf(":")+2, string.lastIndexOf("."));
             y1 = string.substring(6);
             d1 = string.substring(3,5);
             m1 = string.substring(0,2);
@@ -85,7 +85,7 @@ function sortByDate(){
             var y2 = parseInt(y1);
             date1 = new Date(y2, m2, d2, 0, 0, 0, 0);
             var string1 = b[i+1].innerHTML;
-            string1 = string1.substring(string1.indexOf(":")+2, string1.indexOf("."));
+            string1 = string1.substring(string1.lastIndexOf(":")+2, string1.lastIndexOf("."));
             y3 = string1.substring(6);
             d3 = string1.substring(3,5);
             m3 = string1.substring(0,2);
@@ -107,4 +107,50 @@ function sortByDate(){
 
     }
     
+}
+
+function sortByDateOld(){
+  sortListAZ();
+  var list, i, b, y1, m1, d1;
+  list = document.getElementById("myUL");
+  switching = true;
+  while(switching)
+  {
+      switching = false;
+      b = list.getElementsByTagName("li");
+      for(i = 0; i < (b.length-1); i++)
+      {
+          shouldSwitch = false;
+          var string = b[i].innerHTML;
+          string = string.substring(string.lastIndexOf(":")+2, string.lastIndexOf("."));
+          y1 = string.substring(6);
+          d1 = string.substring(3,5);
+          m1 = string.substring(0,2);
+          var d2 = parseInt(d1);
+          var m2 = parseInt(m1);
+          var y2 = parseInt(y1);
+          date1 = new Date(y2, m2, d2, 0, 0, 0, 0);
+          var string1 = b[i+1].innerHTML;
+          string1 = string1.substring(string1.lastIndexOf(":")+2, string1.lastIndexOf("."));
+          y3 = string1.substring(6);
+          d3 = string1.substring(3,5);
+          m3 = string1.substring(0,2);
+          var d4 = parseInt(d3);
+          var m4 = parseInt(m3);
+          var y4 = parseInt(y3);
+          date2 = new Date(y4, m4, d4, 0, 0, 0, 0);
+          if(date1-date2 > 0) 
+          {
+              shouldSwitch = true;
+              break;
+          }
+      }
+      if (shouldSwitch) 
+      {
+          b[i].parentNode.insertBefore(b[i + 1], b[i]);
+          switching = true;
+      }
+
+  }
+  
 }
